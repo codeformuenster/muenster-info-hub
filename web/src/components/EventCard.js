3ef6c3f26796d9fe291b3b3456429c83510bdc1f
ubergesundheit <ubergesundheit@googlemail.com>
2019-09-27 21:21:56 +0200

    EventCard Style changes

--- web/src/components/EventCard.js
+++ web/src/components/EventCard.js
@@ -1,35 +1,39 @@
 import React from "react";
 import Paper from "@material-ui/core/Paper";
-import './EventCard.css'
+import Typography from "@material-ui/core/Typography";
+import "./EventCard.css";
 
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    padding: theme.spacing(3, 2),
-    margin: theme.spacing(2, 2),
-    display: 'grid',
-
+    padding: theme.spacing(1),
+    margin: theme.spacing(2, 2)
   }
 }));
 
-function EventCard({title, time, place, tags, image, description, lat, lon}) {
+function EventCard({ title, time, place, tags, image, description, lat, lon }) {
   const classes = useStyles();
 
   const [isExpanded, setExpanded] = React.useState(false);
 
   return (
-    <Paper
-      className={classes.root}>
+    <Paper className={classes.root}>
       <div className="grid-container">
-        <div className="title">{title}</div>
+        <div className="title">
+          <Typography variant="h5" component="h2" gutterBottom={2}>
+            {title}
+          </Typography>
+        </div>
         <div className="info">
           <div className="stats">
             <div className="time">{time}</div>
             <div className="place">{place}</div>
-    {tags.map(tag =>
-    <div className="tags">{tag}</div>
-    )}
+            <div className="tags">
+              {tags.map(tag => (
+                <span key={tag}>{tag}</span>
+              ))}
+            </div>
           </div>
           <div className="image">
             <img className="event-image" src={image} alt="das Bild" />
