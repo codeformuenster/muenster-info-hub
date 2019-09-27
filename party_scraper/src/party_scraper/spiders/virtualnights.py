f23dba494f3fbd4c281a53d305483eddde75007f
Nicholas Kappel <nicholaskappel@prezap.de>
2019-09-27 21:57:43 +0200

    party_scraper restructure

--- party_scraper/src/party_scraper/spiders/virtualnights.py
+++ party_scraper/src/party_scraper/spiders/virtualnights.py
@@ -56,6 +56,7 @@ class VirtualNightsSpider(scrapy.Spider):
                     'p[@itemprop="location"]/span[@itemtype="http://schema.org/GeoCoordinates"]/meta[@itemprop="latitude"]/@content').get()
 
                 event['location'] = {'lat': geo_lat, 'lon': geo_lon}
+                event['tags'] = ['party']
 
                 img = (article_selector.xpath('div/span/img/@src').get()
                     if article_selector.xpath('div/span/img/@src').get()
