a6023d883cf0bc701cabbd07cbf9bbd792044cd7
ubergesundheit <ubergesundheit@googlemail.com>
2019-09-28 12:31:44 +0200

    expansionpanel

--- web/src/components/EventCard.js
+++ web/src/components/EventCard.js
@@ -17,6 +17,11 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
@@ -31,11 +36,22 @@ const useStyles = makeStyles(theme => ({
     fontWeight: theme.typography.fontWeightLight
   },
   media: {
-    height: 100
+    height: 140
   },
   cardContent: {
     padding: theme.spacing(1, 2)
   },
+  actions: {
+    justifyContent: "center"
+  },
+  cardContentDetails: {
+    height: 0,
+    transition: "height 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms"
+  },
+  detailsExpanded: {
+    height: "auto",
+    transition: "height 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms"
+  }
 }));
 
 function EventCard({ title, time, place, tags, image, description, lat, lon }) {
@@ -47,7 +63,12 @@ function EventCard({ title, time, place, tags, image, description, lat, lon }) {
     <Card className={classes.card} raised={true}>
       <CardMedia className={classes.media} image={image} title={title} />
       <CardContent className={classes.cardContent}>
-        <Typography gutterBottom variant="h5" component="h2" className={classes.title}>
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="h2"
+          className={classes.title}
+        >
           {title}
         </Typography>
         <Typography variant="body1" color="textSecondary" component="p">
@@ -73,14 +94,19 @@ function EventCard({ title, time, place, tags, image, description, lat, lon }) {
           </List>
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
+      <ExpansionPanel>
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            {description}
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
     </Card>
   );
 }
