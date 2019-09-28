07e7fec210492c293920bf86bfb9a7a71b8e5618
ubergesundheit <ubergesundheit@googlemail.com>
2019-09-28 11:23:51 +0200

    change event-cards to separate image and info

--- web/src/components/EventCard.js
+++ web/src/components/EventCard.js
@@ -1,29 +1,41 @@
 import React from "react";
-import * as moment from 'moment';
-import 'moment/locale/de';
-import Paper from "@material-ui/core/Paper";
+import * as moment from "moment";
+import "moment/locale/de";
 import Typography from "@material-ui/core/Typography";
-import RoomIcon from '@material-ui/icons/Room';
+import RoomIcon from "@material-ui/icons/Room";
 import ClockIcon from "@material-ui/icons/WatchLater";
-import LabelIcon from '@material-ui/icons/Label';
+import LabelIcon from "@material-ui/icons/Label";
 
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
 
-import "./EventCard.css";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
 
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    padding: theme.spacing(1),
-    margin: theme.spacing(2, 2),
-    color: "white"
+  card: {
+    margin: theme.spacing(2)
   },
   tag: {
     fontVariant: "small-caps"
   },
   title: {
-    letterSpacing: 1
-  }
+    letterSpacing: 1,
+    fontWeight: theme.typography.fontWeightLight
+  },
+  media: {
+    height: 100
+  },
+  cardContent: {
+    padding: theme.spacing(1, 2)
+  },
 }));
 
 function EventCard({ title, time, place, tags, image, description, lat, lon }) {
@@ -32,43 +44,44 @@ function EventCard({ title, time, place, tags, image, description, lat, lon }) {
   const [isExpanded, setExpanded] = React.useState(false);
 
   return (
-    <Paper
-      className={classes.root}
-      elevation={3}
-      style={{
-        background: `linear-gradient( rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6) ), url(${image})`,
-        backgroundSize: 'cover'
-      }}
-    >
-      <div className="grid-container">
-        <div className="title">
-          <Typography
-            variant="h6"
-            component="h2"
-            gutterBottom
-            className={classes.title}
-          >
-            {title.length > 50 ? <>{title.substring(0, 50)}&hellip;</> : title}
-          </Typography>
-        </div>
-        <div className="info">
-          <div className="stats">
-            <div className="time">
-              <ClockIcon /> {moment(time).fromNow()}
-            </div>
-            <div className="place">
-              <RoomIcon /> {place}</div>
-            <div className="tags">
-              {tags.map(tag => (
-                <span key={tag} className={classes.tag}>
-                  <LabelIcon /> {tag}
-                </span>
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
-    </Paper>
+    <Card className={classes.card} raised={true}>
+      <CardMedia className={classes.media} image={image} title={title} />
+      <CardContent className={classes.cardContent}>
+        <Typography gutterBottom variant="h5" component="h2" className={classes.title}>
+          {title}
+        </Typography>
+        <Typography variant="body1" color="textSecondary" component="p">
+          <List disablePadding={true}>
+            <ListItem dense={true}>
+              <ListItemIcon>
+                <ClockIcon />
+              </ListItemIcon>
+              <ListItemText primary={moment(time).fromNow()} />
+            </ListItem>
+            <ListItem dense={true}>
+              <ListItemIcon>
+                <RoomIcon />
+              </ListItemIcon>
+              <ListItemText primary={place} />
+            </ListItem>
+            <ListItem dense={true}>
+              <ListItemIcon>
+                <LabelIcon />
+              </ListItemIcon>
+              <ListItemText primary={tags.join(", ")} />
+            </ListItem>
+          </List>
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
   );
 }
 
