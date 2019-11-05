b15dfabe862298c45a7a03b72688744a485e1635
Thomas Werner <tom@input23.de>
2019-11-05 19:35:42 +0100

    Scrape first 4 pages instead of only 1 page of results

--- import/meinestadt/src/scrape.js
+++ import/meinestadt/src/scrape.js
@@ -11,21 +11,32 @@ console.log(`index ${index}`);
 
 const client = new Client({ node: elasticUrl });
 
-const buildEventUrl = (from, until) => {
+const buildEventUrl = (from, until, page) => {
 	const fromDate = moment(from).format('DD-MM');
 	const toDate = moment(until).format('DD-MM');
-	return `https://veranstaltungen.meinestadt.de/muenster-westfalen/alle/alle/kalender/d-${fromDate}t-${toDate}?words=`;
+	let url = `https://veranstaltungen.meinestadt.de/muenster-westfalen/alle/alle/kalender/d-${fromDate}t-${toDate}?words=`;
+	if (page && page > 0) {
+		url = url + `&page=${page}`
+	}
+	return url;
 };
 
 const scrapeEventCalendar = async (html) => eventCalendar.extract(html);
-const persist = (body) => client.index({ index, body });
+const persist = (body) => client.index({ index, body, id: body.id });
 
 const scrape = async () => {
+	await scrapePage(0);
+	await scrapePage(2);
+	await scrapePage(3);
+	await scrapePage(4);
+};
+
+const scrapePage = async (page) => {
 	const today = moment();
 	const tomorrow = moment().add(7, 'days');
-	const url = buildEventUrl(today, tomorrow);
+	const url = buildEventUrl(today, tomorrow, page);
 	// eslint-disable-next-line no-console
-	console.log(`requesting ${url}`);
+	console.log(`==> requesting ${url}`);
 	const html = await request(url);
 	const result = await scrapeEventCalendar(html);
 	// eslint-disable-next-line no-console
@@ -34,7 +45,7 @@ const scrape = async () => {
 	await Promise.all(result.map(persist));
 
 	// eslint-disable-next-line no-console
-	console.log(`done indexing ${result.length} items`);
+	console.log(`done indexing ${result.length} items on page ${page}\n`);
 };
 
 module.exports = scrape;
