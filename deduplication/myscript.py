4867e3cc24584df23f8b27f4cc606b8c8a2c469a
Nicholas Kappel <nicholaskappel@prezap.de>
2019-11-16 00:33:32 +0100

    dedup script (WIP)

--- deduplication/myscript.py
+++ deduplication/myscript.py
@@ -1,7 +1,8 @@
 import datetime
 import os
+from itertools import combinations
 
-import editdistance
+import textdistance
 from elasticsearch import Elasticsearch
 
 
@@ -60,4 +61,28 @@ def _normalize_str(string):
 
 if __name__ == "__main__":
     date = datetime.date(year=2019, month=11, day=9)
-    print(_events_by_day(date))
+    # events = [(e['title'], for e in _events_by_day(date)]
+    events = _events_by_day(date)
+
+    # uses lcsseq in relation to length of strings. value < ~1.1 is likely duplicate
+    two_tuples = combinations(events, r=2)
+    two_tuples = [(
+        t[0], t[1], (
+            min(len(t[0]['title']), len(t[1]['title'])) / len(textdistance.lcsseq(t[0]['title'], t[1]['title'])) +
+            min(len(t[0]['location_name']), len(t[1]['location_name'])) / len(textdistance.lcsseq(t[0]['location_name'], t[1]['location_name']))) / 2) for t in two_tuples]
+
+    two_tuples.sort(key=lambda x: x[2])
+
+    for t in two_tuples:
+        print('[{} / {}], [{} / {}] -> {}'
+            .format(
+                t[0]['title'],
+                t[1]['title'],
+                t[0]['location_name'],
+                t[1]['location_name'],
+                t[2]
+            )
+        )
+
+    # TODO: decide which duplicate to delete (prob. the one with "less" data)
+    # TODO: find way of deploying and running (Cronjob?)
